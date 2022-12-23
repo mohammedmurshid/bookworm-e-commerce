76d209564e2eae8e69c317b753955da1442d9569
Mohammed Murshid <murshid434@gmail.com>
2022-12-23 10:50:22 +0530

    order schema added

---
+++ models/order.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose")
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    deliveryAddress: {
+        type: Object
+    },
+    products: [
+        {
+            productId: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product"
+            },
+            quantity: Number,
+            name: String,
+            price: Number,
+            offerPrice: Number,
+        }
+    ],
+    quantity: {
+        type: Number
+    },
+    total: {
+        type: Number
+    },
+    coupen: {
+        code: String,
+        discount: Number
+    },
+    subTotal: {
+        type: Number
+    },
+    paymentType: {
+        type: String,
+        required: true
+    },
+    razorpayOrderId: {
+        type: String
+    },
+    razorpayPaymentId: {
+        type: String
+    },
+    status: {
+        type: String,
+        default: "Pending"
+    }
+}, {timestamps: true })
+
+module.exports = mongoose.model("Order", orderSchema)
